dd38ded031368d1e99109bf74fb22ab1ebb3b210
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-06-11 12:25:52 +0700

    add Text.tsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { TaskProvider } from "./TaskContext"
-import Header from "./components/Header.js";
+import Header from "./components/Header";
 
 function App() {
   return (
-    
       <div className="grid grid-cols-4 gap-4 p-7">  
         <TaskProvider>
           <Header className="rounded-lg	flex bg-yellow-500" name="To Do" boardName="todo" />     
@@ -12,8 +11,7 @@ function App() {
           <Header className="rounded-lg	flex bg-yellow-400" name="Done" boardName="done" />
           <Header className="rounded-lg	flex bg-blue-500" name="Approve" boardName="approve" />
         </TaskProvider>  
-      </div>
-    
+      </div>  
   );
 }
 
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,27 +1,37 @@
 import React from "react";
 import Button from "./Button";
 
-export default function Text ({ id, text, onDelete, moveNext, boardName }) {
+interface TextProps {
+    id: number
+    text: string
+    onDelete:() => void
+    moveNext:() => void
+    boardName: string
+}
+
+const Text:React.FC<TextProps> = ({ id, text, onDelete, moveNext, boardName }) => {
     return (
         <>
             <div className="w-full bg-pink-50 rounded-lg p-3 my-2 flex justify-center">
                 {boardName === "todo" ? "" : 
                     <div className="inline-block fixed mr-72">
-                        <Button name="<" id={id} moveNext={moveNext} />
+                        <Button name="<" id={id} moveNext={moveNext} onDelete={onDelete} />
                     </div>
                 }
                 <div className="inline-block">
                     <p key={id} className="inline-block align-middle">{text}</p>
                 </div>
                 <div className="inline-block fixed ml-52">
-                    <Button name="X" id={id} onDelete={onDelete} />
+                    <Button name="X" id={id} onDelete={onDelete} moveNext={moveNext} />
                 </div> 
                 {boardName === "approve" ? "" :
                     <div className="inline-block fixed ml-72">
-                        <Button name=">" id={id} moveNext={moveNext} />
+                        <Button name=">" id={id} moveNext={moveNext} onDelete={onDelete} />
                     </div>
                 } 
             </div>
         </>
     )
 }
+
+export default Text
